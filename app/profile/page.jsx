02f131bbaa8f624238ad28606af9056b2214c8cb
Mylena <131671711+mylena-dlc@users.noreply.github.com>
2024-10-18 11:43:51 +0200

    page profil

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,38 +1,17 @@
-//   "use client"
+  "use client"
 
-// import React, { useEffect, useState } from 'react';
-// import RecipeCard from '@/components/RecipeCard';
+import React, { useEffect, useState } from 'react';
 
-// const Profile = () => {
-//     const [favorites, setFavorites] = useState<Recipe[]>([]);
+const Profile = () => {
 
-//     useEffect(() => {
-//         const fetchFavorites = () => {
-//             const favoritesFromStorage = JSON.parse(localStorage.getItem('favorites') || '[]');
-//             setFavorites(favoritesFromStorage);
-//         };
-
-//         fetchFavorites();
-//     }, []);
-
-
-//     return (
-//       <div>
-//         <h1>Mon compte</h1>
-//   <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-//                 {favorites.length > 0 ? (
-//                     favorites.map((recipe) => (
-//                         <RecipeCard key={recipe.id} recipe={recipe} />
-//                     ))
-//                 ) : (
-//                     <p>Aucune recette favorite.</p>
-//                 )}
-//             </div>
-//       </div>
-//     );
-//   };
+    return (
+      <div>
+        <h1>Mon compte</h1>
+      </div>
+    );
+  };
   
-//   export default Profile;
+  export default Profile;
 
 
 
